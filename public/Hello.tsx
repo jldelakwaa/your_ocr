4a8de5f345fcb8a8712db181b1cdc1047362e069
Jerald Sto. Domingo <jeraldsto.domingo.coredev@gmail.com>
2026-02-18 15:26:08 +0800

    initial

---
+++ public/Hello.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hello() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your OCR application.</p>
+    </div>
+  );
+}
+export const metadata = {
+  title: "Hello",
+};
